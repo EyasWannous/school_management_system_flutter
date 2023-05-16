3972a391ded324ad086e14faa7ed3261ebde099c
Eyas <eyaswannous@gamil.com>
2023-05-16 02:58:54 -0400

    Adding textTheme to theme and fix darkTheme issue.

--- teacher_app/lib/themes/light_dark_themes.dart
+++ teacher_app/lib/themes/light_dark_themes.dart
@@ -6,6 +6,10 @@ class LightDarkTheme {
     primaryColor: Colors.blue,
     colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.red),
     fontFamily: 'Roboto',
+    textTheme: const TextTheme(
+      headline1: headline1,
+      bodyText1: bodyText1,
+    ),
     useMaterial3: true,
     // Add more attributes as needed
   );
@@ -13,9 +17,25 @@ class LightDarkTheme {
   static ThemeData darkTheme = ThemeData(
     brightness: Brightness.dark,
     primaryColor: Colors.deepPurple,
-    colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.orange),
+    colorScheme: ColorScheme.fromSwatch()
+        .copyWith(secondary: Colors.orange, brightness: Brightness.dark),
     fontFamily: 'Roboto',
+    textTheme: const TextTheme(
+      headline1: headline1,
+      bodyText1: bodyText1,
+    ),
     useMaterial3: true,
     // Add more attributes as needed
+  );
+
+  static const TextStyle headline1 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+  );
+
+  static const TextStyle bodyText1 = TextStyle(
+    fontSize: 16,
+    color: Colors.black87,
   );
 }
