273704fe8d4230c1e5767d94b456ebe37847f600
EyasWannous <eyaswannous@gmail.com>
2023-08-12 19:42:20 -0400

    forget to upload schedule controller

--- student_app/lib/controller/schedulecontroller.dart
+++ student_app/lib/controller/schedulecontroller.dart
@@ -8,39 +8,59 @@ import 'package:student_app/model/schedulemodel.dart';
 import 'package:student_app/model/studentmodel.dart';
 
 import 'package:get/get.dart';
+import 'package:student_app/models/program_model.dart';
+import 'package:student_app/parent/screen/report.dart';
 
 import '../model/AboutsubModel.dart';
+import '../services/rest_api_get.dart';
 
 class HomePageController extends GetxController {
-  List scheduleList = <ScheduleModel>[];
-  late bool isLoading = true;
+  // List scheduleList = <ScheduleModel>[];
+  bool isLoading = true;
+  ProgramModel schedule = ProgramModel();
 
-  final Rx<HomePageController?> schedule = Rx<HomePageController?>(null);
-  ScheduleData scheduleData = ScheduleData(Get.find());
+  // final Rx<HomePageController?> schedule = Rx<HomePageController?>(null);
+  // ScheduleData scheduleData = ScheduleData(Get.find());
   @override
-  void onInit() {
-    getScheduleData();
+  void onInit() async {
+    await fetchDaysData();
     super.onInit();
   }
 
-  void getScheduleData() async {
-    try {
-      //aboutsubList.clear();
-      isLoading = true;
-      update();
-      print("cccccccccccccccccccccccccccccccccccccccc");
-      var response = await scheduleData.getData();
-      print(
-          "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-      isLoading = false;
-      if (response["status"]) {
-        for (var data in response["schedule"]) {
-          scheduleList.add(ScheduleModel.fromJson(data));
-          print(data);
-        }
-      }
-    } catch (e) {
-      print(e);
-    }
+  fetchDaysData() async {
+    isLoading = true;
+    update();
+
+    schedule = await RestAPIGet.getprogram();
+    // print('----------------------');
+    // print(schedule.sunday!.length);
+    // print(schedule.monday!.length);
+    // print(schedule.tuesday!.length);
+    // print(schedule.wednesday!.length);
+    // print(schedule.thursday!.length);
+
+    isLoading = false;
+    update();
   }
+
+  // void getScheduleData() async {
+  //   try {
+  //     //aboutsubList.clear();
+  //     isLoading = true;
+  //     update();
+  //     print("cccccccccccccccccccccccccccccccccccccccc");
+  //     var response = await scheduleData.getData();
+  //     print(
+  //         "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+  //     isLoading = false;
+  //     if (response["status"]) {
+  //       for (var data in response["schedule"]) {
+  //         scheduleList.add(ScheduleModel.fromJson(data));
+  //         print(data);
+  //       }
+  //     }
+  //   } catch (e) {
+  //     print(e);
+  //   }
+  // }
 }
